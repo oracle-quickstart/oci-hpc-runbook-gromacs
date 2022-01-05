da6531f80d7a6a53f98a042b49c60a454de3b7e3
Gloria Lee <glorialee820@gmail.com>
2022-01-05 09:05:21 -0500

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 ## Copyright © 2022, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 
-variable "region" {}
 variable "tenancy_ocid" {}
+variable "region" {}
+variable "fingerprint" {}
+variable "user_ocid" {}
+variable "private_key_path" {}
 variable "targetCompartment" {}
 variable "ad" {}
 variable "ssh_key" {}
 variable "cluster_network" { default = true }
-variable "use_custom_name" { default = false }
+variable "use_custom_name" {}
 variable "cluster_name" { default = "" }
 variable "bastion_ad" {}
 variable "bastion_shape" {}
